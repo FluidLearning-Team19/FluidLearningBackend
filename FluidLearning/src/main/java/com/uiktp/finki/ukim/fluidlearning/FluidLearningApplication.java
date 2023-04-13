d3e6e371b96e12a737add77d1400a97c60772ce1
Andrej Shumkovski <85301264+Shume00@users.noreply.github.com>
2023-04-13 21:52:39 +0200

    Initial Commit
    -initial project

---
+++ FluidLearning/src/main/java/com/uiktp/finki/ukim/fluidlearning/FluidLearningApplication.java
@@ -0,0 +1,13 @@
+package com.uiktp.finki.ukim.fluidlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluidLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluidLearningApplication.class, args);
+    }
+
+}
---
+++ FluidLearning/src/test/java/com/uiktp/finki/ukim/fluidlearning/FluidLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uiktp.finki.ukim.fluidlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluidLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
