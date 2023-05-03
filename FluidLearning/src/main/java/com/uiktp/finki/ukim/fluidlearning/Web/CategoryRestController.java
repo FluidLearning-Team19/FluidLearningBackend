aa2d0fd71624e12b1bb1a4e7d697c20361a6d9f7
Andrej Shumkovski <85301264+Shume00@users.noreply.github.com>
2023-05-03 21:30:18 +0200

    Hotfix for Category endpoint

--- FluidLearning/src/main/java/com/uiktp/finki/ukim/fluidlearning/Web/CategoryRestController.java
+++ FluidLearning/src/main/java/com/uiktp/finki/ukim/fluidlearning/Web/CategoryRestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/data/category")
+@RequestMapping("/api/category")
 @CrossOrigin(value = "*")
 public class CategoryRestController {
     private final CategoryService categoryService;
