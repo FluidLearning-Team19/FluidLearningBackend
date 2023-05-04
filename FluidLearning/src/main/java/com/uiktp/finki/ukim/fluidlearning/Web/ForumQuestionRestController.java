9191749dd7516e2bf361ac304b72233929172003
Damjan77 <80587900+Damjan77@users.noreply.github.com>
2023-05-04 22:44:17 +0200

    added import

--- FluidLearning/src/main/java/com/uiktp/finki/ukim/fluidlearning/Web/ForumQuestionRestController.java
+++ FluidLearning/src/main/java/com/uiktp/finki/ukim/fluidlearning/Web/ForumQuestionRestController.java
@@ -2,7 +2,9 @@ package com.uiktp.finki.ukim.fluidlearning.Web;
 
 import com.uiktp.finki.ukim.fluidlearning.Models.Entities.ForumQuestion;
 import com.uiktp.finki.ukim.fluidlearning.Models.dto.ForumQuestionDto;
+import com.uiktp.finki.ukim.fluidlearning.Models.dto.ReplyDto;
 import com.uiktp.finki.ukim.fluidlearning.Service.ForumQuestionService;
+import com.uiktp.finki.ukim.fluidlearning.Service.ReplyService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
