c9809c1b763f3ba09edd8f11f2d98236aae7da18
Niki <nikolamasalkovski@gmail.com>
2023-05-22 23:55:09 +0200

    Modified ExamDto

---
+++ FluidLearning/src/main/java/com/uiktp/finki/ukim/fluidlearning/Models/dto/ExamWithIdDto.java
@@ -0,0 +1,20 @@
+package com.uiktp.finki.ukim.fluidlearning.Models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExamWithIdDto {
+
+    private String id;
+    private String code;
+    private String title;
+    private String description;
+    private Integer totalTime;
+    private Integer totalPoints;
+    private Integer numberOfQuestions;
+    private CourseDto courseDto;
+}
